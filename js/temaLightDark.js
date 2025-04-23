b1ad39f220a5285e6e2715a9482dca0d468ad159
Lucas Fiaes <lm3355899@gmail.com>
2025-04-22 22:46:34 -0400

    iniciei modo escuro

---
+++ js/temaLightDark.js
@@ -0,0 +1,24 @@
+let trilho = document.getElementById('trilho');
+let body = document.querySelector('body');
+let header = document.getElementById('header');
+let introducao = document.getElementById('introducao');
+let resumo = document.getElementById('resumo');
+let experiencia = document.getElementById('experiencia');
+let formacao = document.getElementById('formacao');
+let contato = document.getElementById('contato');
+
+let titulo = document.getElementsByClassName('h1');
+
+trilho.addEventListener('click', ()=>{
+    trilho.classList.toggle('dark');
+    body.classList.toggle('dark');
+    header.classList.toggle('dark');
+    resumo.classList.toggle('dark');
+    introducao.classList.toggle('dark');
+    formacao.classList.toggle('dark');
+
+    experiencia.classList.toggle('light');
+    contato.classList.toggle('light');
+
+    titulo.classList.toggle('dark');
+})
