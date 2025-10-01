27d889a5692fee462ef5298fed9fce67eff29983
Lucas Fiaes <lm3355899@gmail.com>
2025-10-01 13:42:52 -0400

    jogo da veia adicionado nos projetos

--- js/temaLightDark.js
+++ js/temaLightDark.js
@@ -1,22 +1,37 @@
-let trilho = document.getElementById('trilho');
-let body = document.querySelector('body');
-let header = document.getElementById('header');
-let introducao = document.getElementById('introducao');
-let resumo = document.getElementById('resumo');
-let experiencia = document.getElementById('experiencia');
-let formacao = document.getElementById('formacao');
-let contato = document.getElementById('contato');
+let trilho = document.getElementById("trilho");
+let body = document.querySelector("body");
+let header = document.getElementById("header");
+let introducao = document.getElementById("introducao");
+let resumo = document.getElementById("resumo");
+let experiencia = document.getElementById("experiencia");
+let formacao = document.getElementById("formacao");
+let contato = document.getElementById("contato");
 
-let titulo = document.getElementsByClassName('h1');
+let titulo = document.getElementsByClassName("h1");
 
-trilho.addEventListener('click', ()=>{
-    trilho.classList.toggle('dark');
-    body.classList.toggle('dark');
-    header.classList.toggle('dark');
-    resumo.classList.toggle('dark');
-    introducao.classList.toggle('dark');
-    formacao.classList.toggle('dark');
+let tema = false;
+trilho.addEventListener("click", () => {
+  tema = !tema;
+  localStorage.setItem("dark", tema);
+  trilho.classList.toggle("dark");
+  body.classList.toggle("dark");
+  header.classList.toggle("dark");
+  resumo.classList.toggle("dark");
+  introducao.classList.toggle("dark");
+  formacao.classList.toggle("dark");
 
-    experiencia.classList.toggle('light');
-    contato.classList.toggle('light');
-})
+  experiencia.classList.toggle("light");
+  contato.classList.toggle("light");
+});
+
+if (tema) {
+  trilho.classList.toggle("dark");
+  body.classList.toggle("dark");
+  header.classList.toggle("dark");
+  resumo.classList.toggle("dark");
+  introducao.classList.toggle("dark");
+  formacao.classList.toggle("dark");
+
+  experiencia.classList.toggle("light");
+  contato.classList.toggle("light");
+}
