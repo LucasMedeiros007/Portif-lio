44ae6f30754dd9e0696c2323d32722509a059d37
LucasMedeiros007 <lm3355899@gmail.com>
2025-10-01 19:07:51 -0400

    localstorage com o tema lightDark adicionado e icone na pagina colocado

--- js/temaLightDark.js
+++ js/temaLightDark.js
@@ -1,37 +1,55 @@
-let trilho = document.getElementById("trilho");
-let body = document.querySelector("body");
-let header = document.getElementById("header");
-let introducao = document.getElementById("introducao");
-let resumo = document.getElementById("resumo");
-let experiencia = document.getElementById("experiencia");
-let formacao = document.getElementById("formacao");
-let contato = document.getElementById("contato");
+let trilho = document.getElementById('trilho');
+let body = document.querySelector('body');
+let header = document.getElementById('header');
+let introducao = document.getElementById('introducao');
+let resumo = document.getElementById('resumo');
+let experiencia = document.getElementById('experiencia');
+let formacao = document.getElementById('formacao');
+let contato = document.getElementById('contato');
 
-let titulo = document.getElementsByClassName("h1");
+let titulo = document.getElementsByClassName('h1');
 
-let tema = false;
-trilho.addEventListener("click", () => {
-  tema = !tema;
-  localStorage.setItem("dark", tema);
-  trilho.classList.toggle("dark");
-  body.classList.toggle("dark");
-  header.classList.toggle("dark");
-  resumo.classList.toggle("dark");
-  formacao.classList.toggle("dark");
+let tema;
+function theme() {
+  // localStorage.setItem('tema', 'dark');
+  trilho.classList.toggle('dark');
+  body.classList.toggle('dark');
+  header.classList.toggle('dark');
+  resumo.classList.toggle('dark');
+  formacao.classList.toggle('dark');
 
-  experiencia.classList.toggle("light");
-  contato.classList.toggle("light");
-  introducao.classList.toggle("light");
-});
+  experiencia.classList.toggle('light');
+  contato.classList.toggle('light');
+  introducao.classList.toggle('light');
+}
 
-if (tema) {
-  trilho.classList.toggle("dark");
-  body.classList.toggle("dark");
-  header.classList.toggle("dark");
-  resumo.classList.toggle("dark");
-  formacao.classList.toggle("dark");
+let s = 0;
+function verTheme() {
+  if (localStorage.getItem('tema', 'dark') == 'dark') {
+    localStorage.removeItem('tema');
+    localStorage.setItem('tema', 'light');
+  } else {
+    localStorage.removeItem('tema');
+    localStorage.setItem('tema', 'dark');
+  }
+}
+if (localStorage.getItem('tema', 'dark') == 'dark') {
+  s = 1;
+  // localStorage.removeItem('tema');
+  // localStorage.setItem('tema', 'light');
+} else {
+  s = 0;
+}
 
-  experiencia.classList.toggle("light");
-  contato.classList.toggle("light");
-  introducao.classList.toggle("light");
+trilho.addEventListener('click', theme);
+trilho.addEventListener('click', verTheme);
+
+// if (localStorage.getItem('tema', 'dark') == 'dark') {
+// }
+
+if (s == 1) {
+  theme();
+} else {
+  localStorage.removeItem('tema');
+  localStorage.setItem('tema', 'light');
 }
